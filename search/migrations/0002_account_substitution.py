362cf9185e5f2172796ff78b483bb84bafefac50
unknown <EtudesInfo001@gmail.com>
2019-10-08 03:15:44 +0200

    Deploy

--- search/migrations/0002_account_substitution.py
+++ search/migrations/0002_account_substitution.py
@@ -15,34 +15,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Substitution',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('product', models.CharField(default=None, max_length=255)),
-                ('nutrition_grade', models.CharField(default=None, max_length=2)),
-                ('img_url', models.CharField(default=None, max_length=255)),
-                ('user', models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='Account', to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'db_table': 'Substitution',
-                'ordering': ['id'],
-                'managed': True,
-            },
-        ),
-        migrations.CreateModel(
-            name='Account',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('phone', models.CharField(default=None, max_length=17)),
-                ('date_of_birth', models.DateField(default=None)),
-                ('postal_address', models.CharField(default=None, max_length=25)),
-                ('user', models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='User', to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'db_table': 'Account',
-                'ordering': ['id'],
-                'managed': True,
-            },
-        ),
+
     ]
