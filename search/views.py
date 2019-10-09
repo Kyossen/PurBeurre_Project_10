8fbb18abf9e50afd7581a277fe1aa63752a0674e
Mac-Win-QLF <EtudesInfo001@gmail.com>
2019-10-09 16:17:36 +0200

    Test for error

--- search/views.py
+++ search/views.py
@@ -535,10 +535,10 @@ def favorites(request):
     # If user open favorites page and not add favorites
     else:
         if request.user.is_authenticated:
-            food_all = Substitution.objects.all()
+            food_all = Substitution.objects.filter(
+                user__id=request.session['member_id'])
             # Check if user have already added of the favorites
             if len(food_all) != 0:
-                user_id = request.session['member_id']
                 return display_my_favorites(request, food_all)
             else:
                 context['form_food'] = FoodForm()
